cdb59d40ca64d580fb53e6d25d0f2492c09a5174
Dioni <dsanbla@gmail.com>
2021-04-13 13:09:22 +0200

    Stage 2: Using POST

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
-# Stage 1: What should I do today?
+# Stage 2: First POST
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, inputs
 import sys
+from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
+
+parser.add_argument(
+    'date',
+    type=inputs.date,
+    help="The event date with the correct format is required! The correct format is YYYY-MM-DD!",
+    required=True
+)
+
+parser.add_argument(
+    'event',
+    type=str,
+    help="The event name is required!",
+    required=True
+)
 
 
-# write your code here
 class Event(Resource):
-    def get(self):
-        return {"data": "There are no events for today!"}
+    #def get(self):
+     #   return {"data": "There are no events for today!"}
+
+    def post(self):
+        args = parser.parse_args()
+        date_event = args['date'].strftime("%Y-%m-%d")
+        dict_show = {'message': 'The event has been added!', 'event': args['event'], 'date': date_event}
+        return dict_show
 
 
-api.add_resource(Event, '/event/today')
+api.add_resource(Event, '/event')
 
 # do not change the way you run the program
 if __name__ == '__main__':
